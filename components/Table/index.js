f3c523b82afd8dec8f740bb9964a2fb68b447fd6
Michael Weiner <vikings412@gmail.com>
2023-04-19 09:38:27 -0500

    fix spacing error

--- components/Table/index.js
+++ components/Table/index.js
@@ -77,7 +77,8 @@ export default function Table(props) {
       <div className='space-y-2'>
         <button
           type="button"
-          className="rounded bg-purple-100 px-2 py-1 text-xs font-semibold text-darkpurple shadow-sm hover:bg-purple-200"          onClick={() => {
+          className="rounded bg-purple-100 px-2 py-1 text-xs font-semibold text-darkpurple shadow-sm hover:bg-purple-200"
+          onClick={() => {
             table.resetColumnFilters();
             table.resetSorting();
           }}
