c58a4a195980923f421dc4a8fc7f385f1fb09fea
Michael Weiner <47571709+mike-weiner@users.noreply.github.com>
2023-12-06 12:17:47 -0600

    calculate custom avg_rank field & point it to front-end leaderboard (#57)

--- jobs/pullAocLeaderboards.py
+++ jobs/pullAocLeaderboards.py
@@ -20,6 +20,7 @@ OUTPUT_FILE_PATH = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(
 def main():
   dataToWrite = {}
 
+  # Pull down AoC data via AoC API for each of our private leaderboards
   for leaderboard in LEADERBOARDS:
     response = requests.get(
       f'https://adventofcode.com/{YEAR}/leaderboard/private/view/{leaderboard}.json',
@@ -38,6 +39,28 @@ def main():
 
   dataToWrite['lastUpdatedAt'] = int(time.time())
 
+  # Generate the custom MCC `avg_rank` field
+  member_ids = list(dataToWrite["members"].keys())
+  def completion_time(id, day, part):
+    return dataToWrite["members"][id]['completion_day_level'][day][part]['get_star_ts']
+
+  part_ranks = {}
+  for day in map(str, range(1, 25+1)):
+    for part in ['1', '2']:
+      partid = f"day{day}p{part}"
+      part_ranks[partid] = [
+        id for id in member_ids if day in dataToWrite["members"][id]['completion_day_level'] and part in dataToWrite["members"][id]['completion_day_level'][day]
+      ]
+      part_ranks[partid].sort(key=lambda id: completion_time(id, day, part))
+
+    ranks = {id: [l.index(id)+1 for l in part_ranks.values() if id in l] for id in member_ids}
+    avg_ranks = {id: sum(rankvals)/len(rankvals) if len(rankvals) != 0 else 0 for id, rankvals in ranks.items()}
+
+  # Write the `avg_rank` field to each AoC user
+  for id in avg_ranks:
+    dataToWrite["members"][id]['avg_rank'] = avg_ranks[id]
+
+  # Write the entire AoC data structure to the output file
   with open(OUTPUT_FILE_PATH, 'w', encoding='utf-8') as f:
     json.dump(dataToWrite, f, indent=2)
 
