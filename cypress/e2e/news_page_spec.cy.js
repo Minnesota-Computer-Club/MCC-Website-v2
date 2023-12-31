ab934c5038373e5d518191b048fd1b2b9591e2c6
Michael Weiner <vikings412@gmail.com>
2023-12-31 10:06:46 -0600

    add e2e testing for news page

---
+++ cypress/e2e/news_page_spec.cy.js
@@ -0,0 +1,25 @@
+describe('New Page', () => {
+  beforeEach(() => {
+    cy.visit('/news')
+  });
+
+  it('Has Visible Page Content', () => {
+    cy.get('body').should('be.visible')
+  });
+
+  it('Has At Least 1 Article', () => {
+    cy.get('article').should('have.length.greaterThan', 0);
+  });
+
+  it('Has Articles w/ Content', () => {
+    cy.get('article').each(($article) => {
+      cy.get($article).should('be.visible');
+      cy.get($article).find('time').should('be.visible');
+      cy.get($article).find('h3').should('be.visible');
+      cy.get($article).find('div > p').last().should('be.visible');
+      cy.get($article).click();
+      cy.get('body').contains('Uh, oh. Page not found.').should('not.exist');
+      cy.go('back');
+    });
+  });
+});
